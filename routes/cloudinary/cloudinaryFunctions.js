a1d71c46260ce618f6dc57467a17db21390e1df2
PSCoder10462 <pscoder10462@gmail.com>
2021-03-25 10:41:14 +0530

    destroy image

--- routes/cloudinary/cloudinaryFunctions.js
+++ routes/cloudinary/cloudinaryFunctions.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import { CLOUDINARY_SECRET } from "../../keys.js";
+import { CLOUDINARY_SECRET, API_KEY } from "../../keys.js";
 
 export const signature = (req, res) => {
   try {
@@ -11,4 +11,16 @@ export const signature = (req, res) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+export const deleteImage = (req, res) => {
+  const { path } = req.body;
+  cloudinary.uploader
+    .destroy(`whatsapp-mern/${path}`, {
+      api_key: API_KEY,
+      api_secret: CLOUDINARY_SECRET,
+      cloud_name: "pscoder10462",
+    })
+    .then((data) => res.send("Picture deleted successfully!!"))
+    .catch((err) => console.log(err));
 };
--- routes/cloudinary/cloudinaryRoutes.js
+++ routes/cloudinary/cloudinaryRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signature } from "./cloudinaryFunctions.js";
+import { signature, deleteImage } from "./cloudinaryFunctions.js";
 import requireLogin from "../../middlewares/requireLogin.js";
 
 const router = express.Router();
 
 router.post("/signature", requireLogin, signature);
+router.delete("/image", requireLogin, deleteImage);
 
 export default router;
